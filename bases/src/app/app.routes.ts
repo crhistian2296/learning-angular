ac148380f617c9eda36b122c33cb2171a93abafb
Crhistian Alexander Garcia <c.garcia@izertis.com>
2026-06-03 09:39:06 +0200

    feature: initi

---
+++ bases/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { CounterPageComponent } from './pages/counter/counter-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
+
+export const routes: Routes = [
+  { path: '', component: CounterPageComponent },
+  { path: 'hero', component: HeroPageComponent },
+  { path: 'dragonball', component: DragonballPageComponent },
+  { path: '**', redirectTo: '' },
+];
---
+++ bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-dragonball-page',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './dragonball-page.component.html',
+})
+export class DragonballPageComponent {}
